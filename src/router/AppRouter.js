e40c2064a09150efefc8bad95cc0b1ca72d306ef
ferdeolazabal <ferdeolazabal@gmail.com>
2023-06-24 19:45:56 -0300

    Add prettier format
    - Disable ts check errors

--- src/router/AppRouter.js
+++ src/router/AppRouter.js
@@ -1,50 +1,38 @@
-import { useEffect } from "react";
+// @ts-nocheck
+import React, { useEffect } from "react";
 import { useDispatch } from "react-redux";
-import {
-    BrowserRouter,
-    Routes,
-    Route,
-    } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 
-import { Home } from            "../views/Home";
+import { Home } from "../views/Home";
 import { SetValueScreen } from "../views/SetValueScreen";
-import PropertiesEdit from     "../views/PropertiesEdit";
+import PropertiesEdit from "../views/PropertiesEdit";
 import { startLoadingCategories } from "../redux/actions/categories.js";
 import { startLoadingProperties } from "../redux/actions/properties.js";
 import { startLoadingVehicles } from "../redux/actions/vehicle";
 import { GetValues } from "../views/GetValues";
 
-
 export const AppRouter = () => {
+  const dispatch = useDispatch();
 
-    const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(startLoadingVehicles());
+    dispatch(startLoadingCategories());
+    dispatch(startLoadingProperties());
+  }, [dispatch]);
 
-    useEffect( () => {
-        dispatch(startLoadingVehicles());
-        dispatch(startLoadingCategories());
-        dispatch(startLoadingProperties());
-    }, [ dispatch ] );
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="inspection" element={<SetValueScreen />} />
 
-    return (
-        <BrowserRouter>
-            <Routes>
-                <Route path="inspection" element={
-                    <SetValueScreen />
-                } />
+        <Route path="PropertiesEdit" element={<PropertiesEdit />} />
 
-                <Route path="PropertiesEdit" element={
-                    <PropertiesEdit />
-                } />
+        <Route path="vehicle/:id" element={<GetValues />} />
 
-                <Route path="vehicle/:id" element={
-                    <GetValues/>
-                } />
+        <Route path="/" element={<Home />} />
 
-                <Route path="/" element={
-                    <Home />
-                } /> 
-
-            </Routes>
-        </BrowserRouter>
-    );
+        <Route path="/*" element={<Navigate to="/" replace />} />
+      </Routes>
+    </BrowserRouter>
+  );
 };
