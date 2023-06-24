a40b481cb01f8a246a4f449d8de77e90bb042635
ferdeolazabal <ferdeolazabal@gmail.com>
2023-06-24 20:31:49 -0300

    Add prettier format

--- src/views/GetValues.js
+++ src/views/GetValues.js
@@ -1,46 +1,54 @@
-import { useSelector } from 'react-redux';
-import PropertyValue from '../components/PropertyValue';
+// @ts-nocheck
+import React from "react";
 import { useNavigate } from "react-router-dom";
+import { useSelector } from "react-redux";
+import PropertyValue from "../components/PropertyValue";
 
 export const GetValues = () => {
+  const navigate = useNavigate();
+  const { vehicle } = useSelector(({ vehicles }) => vehicles.vehicle);
+  const { getVehicleProperty } = useSelector(
+    ({ property }) => property.properties
+  );
+  const { propertyCategories } = useSelector(
+    ({ categories }) => categories.categories
+  );
 
-    const navigate = useNavigate()
-    // @ts-ignore
-    const { vehicle } = useSelector( state => state.vehicles.vehicle );
-    // @ts-ignore
-    const { getVehicleProperty } = useSelector( state => state.property.properties );
-    // @ts-ignore
-    const { propertyCategories } = useSelector( state => state.categories.categories );
-
-
-    const values = vehicle && vehicle?.properties?.map( prop => {
-        const property = getVehicleProperty.find( cat => cat.id === prop.vehicle_property_FK );
-        // anexar la categoria
-        const category = propertyCategories.find( cat => cat.id === property.property_category_FK );
-        return {
-            ...prop,
-            category: category.name,
-            property: property.name,
-            value: prop.value || 0
-        }
+  const values =
+    vehicle &&
+    vehicle?.properties?.map((prop) => {
+      const property = getVehicleProperty.find(
+        (cat) => cat.id === prop.vehicle_property_FK
+      );
+      // anexar la categoria
+      const category = propertyCategories.find(
+        (cat) => cat.id === property.property_category_FK
+      );
+      return {
+        ...prop,
+        category: category.name,
+        property: property.name,
+        value: prop.value || 0,
+      };
     });
 
-    const capitalize = ( str = '' ) => {
-        return str.charAt( 0 ).toUpperCase() + str.slice( 1 );
-    };
+  const capitalize = (str = "") => {
+    return str.charAt(0).toUpperCase() + str.slice(1);
+  };
 
-    const handleSubmit  = (e) => {
-        e.preventDefault();
-        navigate('/');
-    }
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    navigate("/");
+  };
 
-    return (
-        <>
-        <div className="container">
-            <h3 className="mt-4 mb-4"> 
-                { capitalize(vehicle?.brand) } { capitalize(vehicle?.model) } { vehicle?.year }
-                <br />
-                    {/* {
+  return (
+    <>
+      <div className="container">
+        <h3 className="mt-4 mb-4">
+          {capitalize(vehicle?.brand)} {capitalize(vehicle?.model)}{" "}
+          {vehicle?.year}
+          <br />
+          {/* {
                         values && values.map( value => (
                             <span key={ value.id } >
                                 <PropertyValue
@@ -51,51 +59,47 @@ export const GetValues = () => {
                             </span>
                         ))
                     } */}
-            </h3>
-            
-            <div className="row">
-                <div className="col-12">
-                    <table className="table table-striped">
-                        <thead>
-                            <tr>
-                                <th>Categoria</th>
-                                <th>Propiedad</th>
-                                <th>Valor</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            {values && values.map( value => (
-                                <tr key={ value.id }>
-                                    <td>{ value.category }</td>
-                                    <td>{ value.property }</td>
-                                    {/* <td>{ value.value }</td> */}
-                                    <PropertyValue
-                                        id={ value.id }
-                                        name={ value.null }
-                                        value={ value.value }
-                                />
-                                </tr>
-                            ))}
-                        </tbody>
-                    </table>
-                </div>
-            </div>
-            <div className="fixed-botto">
-                        < div className="d-grid gap-2 col-5 mx-auto">
-                            
-                            <button 
-                                className="mb-5 btn btn-lg btn-block btn-primary"
-                                type="button"
-                                onClick={ handleSubmit }
-                            >
-                                Volver
-                            </button>
+        </h3>
 
-                        </div>
-                    </div>
+        <div className="row">
+          <div className="col-12">
+            <table className="table table-striped">
+              <thead>
+                <tr>
+                  <th>Categoria</th>
+                  <th>Propiedad</th>
+                  <th>Valor</th>
+                </tr>
+              </thead>
+              <tbody>
+                {values &&
+                  values.map((value) => (
+                    <tr key={value.id}>
+                      <td>{value.category}</td>
+                      <td>{value.property}</td>
+                      <PropertyValue
+                        id={value.id}
+                        name={value.null}
+                        value={value.value}
+                      />
+                    </tr>
+                  ))}
+              </tbody>
+            </table>
+          </div>
         </div>
-
-
-        </>
-    );
+        <div className="fixed-botto">
+          <div className="d-grid gap-2 col-5 mx-auto">
+            <button
+              className="mb-5 btn btn-lg btn-block btn-primary"
+              type="button"
+              onClick={handleSubmit}
+            >
+              Volver
+            </button>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 };
--- src/views/PropertiesEdit.js
+++ src/views/PropertiesEdit.js
@@ -1,35 +1,29 @@
-import { NavBar } from '../components/NavBar';
-import { PropertiesHelper } from '../helpers/propertiesHelper';
+// @ts-nocheck
+import React from "react";
+import { NavBar } from "../components/NavBar";
+import { PropertiesHelper } from "../helpers/propertiesHelper";
 
 const PropertiesEdit = () => {
-
-return (
-
+  return (
     <div className="container">
-        <NavBar />
-        <h1 className='mt-4 mb-4'>Formulario de Inspección</h1>
-        <hr />
-        <div className='row mt-4'>
-            <div className='col-5 mt-3'>
-                <h4>Propiedades</h4>
-                <hr/>
-            </div>
+      <NavBar />
+      <h1 className="mt-4 mb-4">Formulario de Inspección</h1>
+      <hr />
+      <div className="row mt-4">
+        <div className="col-5 mt-3">
+          <h4>Propiedades</h4>
+          <hr />
         </div>
-        <div className="row">
-            <div className="col-12">
-                <table className="table table-striped">
-                    <PropertiesHelper />
-                </table>
-
-            </div>
+      </div>
+      <div className="row">
+        <div className="col-12">
+          <table className="table table-striped">
+            <PropertiesHelper />
+          </table>
         </div>
-
+      </div>
     </div>
-
-
-
-    
-)
+  );
 };
 
 export default PropertiesEdit;
