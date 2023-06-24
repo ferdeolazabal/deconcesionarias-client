050af526c9c8d12a8616f6c35358006964a31814
ferdeolazabal <ferdeolazabal@gmail.com>
2023-06-24 20:31:58 -0300

    Add misssing routes

--- src/router/AppRouter.js
+++ src/router/AppRouter.js
@@ -1,13 +1,13 @@
 // @ts-nocheck
 import React, { useEffect } from "react";
-import { useDispatch } from "react-redux";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import { GetValues } from "../views/GetValues";
 import { Home } from "../views/Home";
 import { SetValueScreen } from "../views/SetValueScreen";
 import { startLoadingCategories } from "../redux/actions/categories.js";
 import { startLoadingProperties } from "../redux/actions/properties.js";
 import { startLoadingVehicles } from "../redux/actions/vehicle";
+import { useDispatch } from "react-redux";
 import PropertiesEdit from "../views/PropertiesEdit";
 
 export const AppRouter = () => {
@@ -26,7 +26,11 @@ export const AppRouter = () => {
 
         <Route path="PropertiesEdit" element={<PropertiesEdit />} />
 
+        <Route path="vehicle/:id" element={<GetValues />} />
+
         <Route path="/" element={<Home />} />
+
+        <Route path="/*" element={<Navigate to="/" replace />} />
       </Routes>
     </BrowserRouter>
   );
