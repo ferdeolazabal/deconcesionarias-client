f86b4889b0ae3c872946eb9a3b8400db4d46f0c3
ferdeolazabal <ferdeolazabal@gmail.com>
2023-06-24 21:15:49 -0300

    Change rating size
    - Disable value edition

--- src/components/PropertyValue.js
+++ src/components/PropertyValue.js
@@ -1,32 +1,25 @@
+// @ts-nocheck
+import React from "react";
 import { Rating } from "@mui/material";
 import { useDispatch } from "react-redux";
 import { valueCreate } from "../redux/actions/value";
 
-const PropertyValue = ( { id, name, value} ) => {
+const PropertyValue = ({ id, value }) => {
+  const dispatch = useDispatch();
 
-    const dispatch = useDispatch()
+  const updateValue = (id, e) => {
+    dispatch(valueCreate(id, e.target.value));
+  };
 
-    const updateValue = ( id, e ) => {
-        dispatch( valueCreate( id, e.target.value ) );
-    };
-
-    return (
-        <div
-            className="content"
-            key={ id }
-        >
-            <div>{ name } </div>
-    
-            <Rating
-                size='small'
-                name={`simple-controlled-${ id }`}
-                value={ value }
-                onChange={(newValue) => {
-                    updateValue( id, newValue );}}
-            />
-
-        </div>
-    )
+  return (
+    <Rating
+      disabled={true}
+      size="large"
+      name={`simple-controlled-${id}`}
+      value={value}
+      onChange={(newValue) => updateValue(id, newValue)}
+    />
+  );
 };
 
 export default PropertyValue;
