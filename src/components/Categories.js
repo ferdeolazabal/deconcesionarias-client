6210e4483eeebd50ee1dae4e537cff2d6bd59257
ferdeolazabal <ferdeolazabal@gmail.com>
2023-06-24 21:19:53 -0300

    Add prettier format

--- src/components/Categories.js
+++ src/components/Categories.js
@@ -1,76 +1,70 @@
 // @ts-nocheck
-import { useState } from 'react';
-import { useSelector } from 'react-redux';
-import { Tabs, Tab, Box } from '@mui/material';
+import { useState } from "react";
+import { useSelector } from "react-redux";
+import { Tabs, Tab, Box } from "@mui/material";
 
-import { Properties } from './Properties';
+import { Properties } from "./Properties";
 
 const indexTab = (index) => ({
-    id: `simple-tab-${index}`,
-    'aria-controls': `simple-tabpanel-${index}`,
-})
+  id: `simple-tab-${index}`,
+  "aria-controls": `simple-tabpanel-${index}`,
+});
 
+const Categories = () => {
+  const { propertyCategories } = useSelector(
+    ({ categories }) => categories.categories
+  );
+  console.log({ propertyCategories });
 
-const Categories = ( props ) => {
-    console.log( 'Categories props', props );
-    // @ts-ignore
-    const { propertyCategories } = useSelector( state => state.categories.categories );
+  const [value, setValue] = useState(0);
 
-    const [ value, setValue ] = useState(0);
-    // @ts-ignore
-    const handleChange = (event, newValue) => {
-        setValue(newValue);
-    };
+  const handleChange = (event, newValue) => {
+    setValue(newValue);
+  };
 
-
-return (
+  return (
     <div className="container">
-        <div className="test">
+      <div className="test">
+        <Box
+          sx={{
+            alignItems: "center",
+            width: "100%",
+            height: "100%",
+          }}
+          mt={3}
+        >
+          <Tabs
+            value={value}
+            onChange={handleChange}
+            textColor="secondary"
+            variant="scrollable"
+            //variant="fullWidth"
+            indicatorColor="secondary"
+            allowScrollButtonsMobile
+            scrollButtons="auto"
+            selectionFollowsFocus
+          >
+            {propertyCategories &&
+              propertyCategories.map((category, i) => (
+                <Tab
+                  icon={
+                    <i className="material-icons outlined md-48 orange600">
+                      {category.icon}
+                    </i>
+                  }
+                  key={category.id}
+                  label={category.name}
+                  {...indexTab(i)}
+                  wrapped
+                />
+              ))}
+          </Tabs>
+        </Box>
+      </div>
 
-            <Box 
-                sx={{ 
-                    alignItems: 'center',
-                    width: '100%',
-                    height: '100%',
-                }}
-                mt={3}
-                >
-                <Tabs
-                    value={value} 
-                    onChange={handleChange} 
-                    textColor="secondary"
-                    variant="scrollable"
-                    //variant="fullWidth"
-                    indicatorColor="secondary"
-                    allowScrollButtonsMobile
-                    scrollButtons="auto"
-                    selectionFollowsFocus
-                >
-                    {
-                        propertyCategories && propertyCategories
-                            .map( (category, i) => 
-                                (
-                                    <Tab 
-                                        icon={<i className="material-icons outlined md-48 orange600">{ category.icon }</i>}
-                                        key={category.id} 
-                                        label={category.name} 
-                                        {...indexTab(i)}
-                                        wrapped
-                                    />
-                                )
-                            )
-                    }
-                </Tabs>
-            </Box>
-
-        </div>
-
-        <Properties
-            property={value}
-        />
-
+      <Properties property={value} />
     </div>
-);
+  );
 };
 
 export default Categories;
